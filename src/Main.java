099dbb93f5331d023892011f6e2c05b1b99f3dbe
HoangHien120398 <hoanghien120398@gmail.com>
2025-04-24 21:18:09 +0700

    String methods

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.print("Hello World! ");
+        System.out.print("I will print on the same line.");
+        System.out.println(3 + 3);
+        System.out.println(2*5);
+        System.out.println(6/3);
+        System.out.println(8%5);
+
+        String greeting = "Hello";
+        String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        System.out.println("The length of the text is: " + txt.length());
+
+        //String methods
+        String txt_1 = "Hello Word";
+        System.out.println(txt_1.toUpperCase());
+        System.out.println(txt_1.toLowerCase());
+
+        String txt_2 = "Please locate where 'locate' occurs!";
+        System.out.println("Position of the 'locate' is: " + txt_2.indexOf("locate"));
     }
 }
